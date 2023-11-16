5535ad0ce29dbe42f232fd9c31e82516f9db72e2
Dennis <kargopolov.d.a@gmail.com>
2023-11-16 16:47:29 +0300

    новая миграция

--- src/resources/views/components/users-list.blade.php
+++ src/resources/views/components/users-list.blade.php
@@ -48,19 +48,13 @@
                             </td>
                             <td class="project-actions text-right">
                                 <a class="btn btn-primary btn-sm" href="#">
-                                    <i class="fas fa-folder">
-                                    </i>
-                                    View
+                                    <i class="fas fa-eye"></i>
                                 </a>
                                 <a class="btn btn-info btn-sm" href="#">
-                                    <i class="fas fa-pencil-alt">
-                                    </i>
-                                    Edit
+                                    <i class="fas fa-pencil-alt"></i>
                                 </a>
                                 <a class="btn btn-danger btn-sm" href="#">
-                                    <i class="fas fa-trash">
-                                    </i>
-                                    Delete
+                                    <i class="fas fa-trash"></i>
                                 </a>
                             </td>
                         </tr>
